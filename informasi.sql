9e8c50dfc076066fa61454bcfee2f0682dcafcf0
ArifFirdausNasapola <43140658+ArifFirdausNasapola@users.noreply.github.com>
2018-09-19 23:56:14 +0700

    Add files via upload

---
+++ informasi.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 19 Sep 2018 pada 12.54
+-- Versi server: 10.1.31-MariaDB
+-- Versi PHP: 7.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ta3`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `informasi`
+--
+
+CREATE TABLE `informasi` (
+  `Nim` int(10) NOT NULL,
+  `Nama` varchar(25) NOT NULL,
+  `Fakultas` varchar(20) NOT NULL,
+  `Jenis Kelamin` varchar(10) NOT NULL,
+  `Nama File/Gambar` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
